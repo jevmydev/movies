196ea2cdb415afc0e8a2bf4a6042493c74a2a117
Jeremy Díaz <jeremy664k@gmail.com>
2023-01-24 13:03:50 -0300

    Fix video hero with youtube-nocookie and loop

--- js/heroMovie.js
+++ js/heroMovie.js
@@ -8,15 +8,13 @@ const $average = $(".hero__average");
 const $votes = $(".hero__vote");
 const $genres = $(".hero__genres");
 
-const refreshToMinute = 60000;
-
 async function showHeroMovie() {
     const heroMovie = movies.results[0];
     const { title, id, vote_average, vote_count, genre_ids } = heroMovie;
 
     const heroVideo = await getVideoMovie(`${id}/videos`, 1, "es");
     const videoKey = heroVideo.key;
-    const srcVideo = `https://www.youtube.com/embed/${videoKey}?controls=0&autoplay=1&mute=1`;
+    const srcVideo = `https://www.youtube-nocookie.com/embed/${videoKey}?controls=0&mute=1&autoplay=1&loop=1&playlist=${videoKey}`;
 
     $title.textContent = title;
     $average.textContent = `${vote_average}/10`;
@@ -25,8 +23,6 @@ async function showHeroMovie() {
 
     const genres = getGenres(genre_ids);
     $genres.innerHTML = genres;
-
-    setInterval(() => $video.src = srcVideo, refreshToMinute);
 }  
 
 showHeroMovie();
