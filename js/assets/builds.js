a6852360299269875b21d0da0dfbc665f6e01f81
Jeremy Díaz <jeremy664k@gmail.com>
2023-01-26 19:02:12 -0300

    Fix stop video and update listeners and variables

--- js/assets/builds.js
+++ js/assets/builds.js
@@ -33,7 +33,7 @@ export function buildMovie({ title, poster_path, vote_average, genre, id }) {
                         <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
                     </svg>
                 </button>
-                <button class="movie__button movie__button--list" title="Añadir a la lista">
+                <button class="movie__button movie__button--list" title="Añadir a la lista" data-id="${id}">
                     <svg width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
                     </svg>
@@ -63,6 +63,7 @@ export function buildIframeMovie(srcVideo) {
     return iframeMovie;
 }
 
-export function refreshListeners(callBackInformation) {
+export function refreshListeners(callBackInformation, callbackList) {
     $$(".movie__button--information").forEach(informationMovie => informationMovie.addEventListener("click", callBackInformation));
+    $$(".movie__button--list").forEach(informationMovie => informationMovie.addEventListener("click", callbackList));
 }
